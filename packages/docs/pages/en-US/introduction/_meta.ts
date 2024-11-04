ea65a383b326e228f96e885e2914c3e3b342acbe
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2024-11-04 20:05:47 +0800

    docs: add compatibility page

--- packages/docs/pages/en-US/introduction/_meta.ts
+++ packages/docs/pages/en-US/introduction/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   'index': 'Univer',
   'concepts': 'Concepts',
+  'compatibility': 'Tech Stack & Compatibility Statement',
   'architecture': 'Architecture',
   'collaborative-benchmark': 'Collaborative Benchmark Testing Report',
   'contributing': 'Contributing',
--- packages/docs/pages/zh-CN/introduction/_meta.ts
+++ packages/docs/pages/zh-CN/introduction/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   'index': 'Univer 简介',
   'concepts': '基础概念',
+  'compatibility': '技术栈与兼容性声明',
   'architecture': '架构',
   'collaborative-benchmark': '协同基准测试报告',
   'contributing': '贡献指南',
