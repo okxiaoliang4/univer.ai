114130bf8aa9804567e7637041eb371e8a815845
zsq1234 <2387311298@qq.com>
2024-11-06 15:17:07 +0800

    feat: add saas document

--- packages/docs/pages/en-US/guides/sheets/pro-features/server/_meta.ts
+++ packages/docs/pages/en-US/guides/sheets/pro-features/server/_meta.ts
@@ -1,8 +1,10 @@
 export default {
   'overview': 'Overview',
-  'docker': 'Deploying to Docker',
-  'kubernetes': 'Deploying to Kubernetes',
+  'docker': 'Deploy to Docker',
+  'kubernetes': 'Deploy to Kubernetes',
   'usip': 'Univer Server Integration Protocol',
   'import-and-export': 'Import And Export',
   'api': 'Univer Server API',
+  'aliyun': 'Deploy to Aliyun',
+  'aws': 'Deploy to AWS',
 }
--- packages/docs/pages/zh-CN/guides/sheets/pro-features/server/_meta.ts
+++ packages/docs/pages/zh-CN/guides/sheets/pro-features/server/_meta.ts
@@ -5,4 +5,6 @@ export default {
   'usip': 'Univer Server 集成协议',
   'import-and-export': '导入导出',
   'api': 'Univer Server 接口定义',
+  'aliyun': '部署到阿里云',
+  'aws': '部署到亚马逊云',
 }
